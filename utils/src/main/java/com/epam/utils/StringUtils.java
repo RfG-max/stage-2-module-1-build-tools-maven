54ae187395855d77e6f7b9ff63a074d57b96923b
1 <test@tut.by>
2024-10-24 20:12:10 +0300

    commit changes

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null) {
+            return false;
+        }
+        try {
+            if(Double.parseDouble(str)>0) return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return false;
     }
 }
